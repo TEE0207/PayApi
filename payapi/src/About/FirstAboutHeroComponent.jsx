aaacd53ff2ddfb7dac24afd469000e275a08a46c
Taofeek Lawal <taofeeklwl@gmail.com>
2025-03-08 19:26:41 +0000

    updated page

--- payapi/src/About/FirstAboutHeroComponent.jsx
+++ payapi/src/About/FirstAboutHeroComponent.jsx
@@ -4,6 +4,10 @@ import tabletPicture from "../../src/assets/about/tablet/image-team-members.jpg"
 import mobilePicture from "../../src/assets/about/mobile/image-team-members.jpg"
 import { IoAdd } from "react-icons/io5";
 import CountUp from 'react-countup';
+import LazyLoad from 'react-lazy-load';
+
+
+
 
 
 export default function FirstAboutHeroComponent(){
@@ -27,11 +31,14 @@ export default function FirstAboutHeroComponent(){
                     </div>
 
                     <div className={styles.imageContainer}>
+                    
                     <picture className={styles.pictureContainer}>
-                        <source media="(max-width: 480px)" srcSet={mobilePicture} />
-                        <source media="(max-width: 1120px)" srcSet={tabletPicture} className={styles.picImage}/>
-                        <img src={desktopPicture}alt="members picture" className={styles.picImage}/>                        
-                    </picture>
+                            <source media="(max-width: 480px)" srcSet={mobilePicture} />
+                            <source media="(max-width: 1120px)" srcSet={tabletPicture} className={styles.picImage}/>
+                            <img loading="eager" src={desktopPicture}alt="members picture" className={styles.picImage}/>                        
+                        </picture>
+
+                
                         {/* <img src={picture}alt="members picture" className={styles.picImage}/> */}
 
 
